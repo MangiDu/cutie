7aedaadfeb3d2d3fccb8f28fd3d6dff271bd3090
Mangidu <duxiaoxue93@gmail.com>
2017-04-18 13:29:24 +0800

    handle error

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,15 @@
 var gulp = require('gulp')
+var plumber = require('gulp-plumber')
 var stylus = require('gulp-stylus')
+var rename = require('gulp-rename')
 var browserSync = require('browser-sync')
 var reload = browserSync.reload
 
 gulp.task('stylus', function () {
-  gulp.src('./src/stylus/portal.styl')
+  gulp.src('./src/stylus/index.styl')
+    .pipe(plumber())
     .pipe(stylus())
+    .pipe(rename('pt.css'))
     .pipe(gulp.dest('./src/css'))
     .pipe(reload({
       stream: true
