ca37892daf860ec10dcb199c25a70717709ee080
Mangidu <duxiaoxue93@gmail.com>
2017-04-20 21:13:02 +0800

    listen to file changes

--- gulpfile.js
+++ gulpfile.js
@@ -11,12 +11,12 @@ var reload = browserSync.reload
 
 gulp.task('clean', function (cb) {
   return del([
-    './dist/**/*'
+    'dist/**/*'
   ], cb)
 })
 
 gulp.task('stylus', function () {
-  return gulp.src('./src/stylus/index.styl')
+  return gulp.src('src/stylus/index.styl')
     .pipe(plumber())
     .pipe(stylus())
     .pipe(rename('index.css'))
@@ -26,37 +26,44 @@ gulp.task('stylus', function () {
         'not ie <= 8'
       ]
     }))
-    .pipe(gulp.dest('./dist/css'))
+    .pipe(gulp.dest('dist/css'))
     .pipe(reload({
       stream: true
     }))
 })
 
 gulp.task('pug', function () {
-  return gulp.src('./src/views/*.pug')
+  return gulp.src('src/views/*.pug')
     .pipe(plumber())
     .pipe(pug({
       doctype: 'html',
-      baseDir: './src/views',
+      baseDir: 'src/views',
       pretty: true
     }))
-    .pipe(gulp.dest('./dist'))
+    .pipe(gulp.dest('dist'))
 })
 
-gulp.task('copy', function () {
-  return gulp.src('./src/vendor/**/*')
-    .pipe(gulp.dest('./dist/vendor'))
+gulp.task('copy:vendor', function () {
+  return gulp.src('src/vendor/**/*')
+    .pipe(gulp.dest('dist/vendor'))
 })
+
+gulp.task('copy:assets', function () {
+  return gulp.src('src/assets/**/*')
+    .pipe(gulp.dest('dist/assets'))
+})
+
+gulp.task('copy', ['copy:vendor', 'copy:assets'])
 
 gulp.task('inject', ['stylus', 'pug'], function () {
-  return gulp.src('./dist/*.html')
+  return gulp.src('dist/*.html')
     .pipe(inject(gulp.src([
-      './dist/vendor/**/*',
-      './dist/css/*.css'
+      'dist/vendor/**/*',
+      'dist/css/*.css'
     ], {
       read: false
     }), {relative: true}))
-    .pipe(gulp.dest('./dist'))
+    .pipe(gulp.dest('dist'))
 })
 
 gulp.task('compile', ['copy', 'stylus', 'pug', 'inject'])
@@ -70,7 +77,10 @@ gulp.task('serve', ['compile'], function() {
   })
 
   gulp.watch('*.html', {cwd: 'dist'}, reload)
-  gulp.watch('./src/stylus/**/*.styl', ['stylus'])
-  gulp.watch('./src/views/**/*.pug', ['pug', 'inject'])
-  gulp.watch('./src/vendor/**/*', ['copy'])
+  gulp.watch('src/stylus/**/*.styl', ['stylus'])
+  gulp.watch('src/views/**/*.pug', ['pug', 'inject'])
+  gulp.watch([
+    'src/vendor/**/*.*',
+    'src/assets/**/*.*'
+  ], ['copy'])
 })
